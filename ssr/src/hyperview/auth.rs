e4b273cf541c235e0e6cdf694b098625a5536700
Rami Jebara <rami.jebara@hyperviewhq.com>
2025-01-28 16:23:26 -0800

    Update packages

--- ssr/src/hyperview/auth.rs
+++ ssr/src/hyperview/auth.rs
@@ -1,24 +1,23 @@
 use crate::AppConfig;
 use anyhow::Result;
-use oauth2::{
-    basic::BasicClient, reqwest::http_client, AuthUrl, ClientId, ClientSecret, Scope,
-    TokenResponse, TokenUrl,
-};
+use oauth2::basic::BasicClient;
+use oauth2::reqwest;
+use oauth2::{AuthUrl, ClientId, ClientSecret, Scope, TokenResponse, TokenUrl};
 
 pub fn get_auth_header(config: &AppConfig) -> Result<String> {
-    // Create client
-    let client = BasicClient::new(
-        ClientId::new(config.client_id.clone()),
-        Some(ClientSecret::new(config.client_secret.clone())),
-        AuthUrl::new(config.auth_url.clone())?,
-        Some(TokenUrl::new(config.token_url.clone())?),
-    );
+    let auth_client = BasicClient::new(ClientId::new(config.client_id.clone()))
+        .set_client_secret(ClientSecret::new(config.client_secret.clone()))
+        .set_auth_uri(AuthUrl::new(config.auth_url.clone())?)
+        .set_token_uri(TokenUrl::new(config.token_url.clone())?);
 
-    // fetch token
-    let token_result = client
+    let http_client = reqwest::blocking::ClientBuilder::new()
+        .redirect(reqwest::redirect::Policy::none())
+        .build()?;
+
+    let token_result = auth_client
         .exchange_client_credentials()
-        .add_scope(Scope::new(config.scope.clone()))
-        .request(http_client)?;
+        .add_scope(Scope::new("HyperviewManagerApi".to_string()))
+        .request(&http_client)?;
 
     Ok(format!("Bearer {}", token_result.access_token().secret()))
 }
